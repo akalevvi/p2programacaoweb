27598a9c85815f417346dcf3e54efb8d709e6b29
Leandro <lleandrovicente@gmail.com>
2016-09-27 00:43:06 -0300

    Instalado e configurado o clearance para autenticação

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926143128) do
+ActiveRecord::Schema.define(version: 20160927025846) do
 
   create_table "client_reviews", force: :cascade do |t|
     t.string   "comment"
@@ -69,6 +69,17 @@ ActiveRecord::Schema.define(version: 20160926143128) do
     t.integer  "photographer_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "email",                          null: false
+    t.string   "encrypted_password", limit: 128, null: false
+    t.string   "confirmation_token", limit: 128
+    t.string   "remember_token",     limit: 128, null: false
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
 end
