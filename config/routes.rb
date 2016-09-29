76e98bea39830c988c5046e0a45d5579d82f1fbf
Leandro <lleandrovicente@gmail.com>
2016-09-28 21:20:33 -0300

    Definiu rota raiz para ser a tela de login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :photographers
   resources :clients
+  root 'clearance/sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
