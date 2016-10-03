a7136357c1d38b079e5e2bccc30bb0bbe331bb15
Leandro <lleandrovicente@gmail.com>
2016-10-03 13:07:11 -0300

    Criou seeds para fotografos e pagina para exibir fotografos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Photographer.create([{name: 'Fernando Borges', display_name: "Fernando Borges", phone: '22 2888-1231', birthdate: '30-03-1990', city: 'Campos', country: 'Brasil',
+  state: 'RJ', email: 'fernando@mail.com', password: 'fernando', avatar: 'http://localhost:3000/uploads/perfil.jpg',
+  bio: 'Casamentos, bodas e batizado', portfolio_link: 'www.flickr.com'}])
+
+Photographer.create([{name: 'Yargo Lima', display_name: "Yargo Lima", phone: '22 2888-1231', birthdate: '30-03-1990', city: 'Campos', country: 'Brasil',
+  state: 'RJ', email: 'yargo@mail.com', password: 'fernando', avatar: 'http://localhost:3000/uploads/perfil.jpg',
+  bio: 'Casamentos, bodas e batizado', portfolio_link: 'www.flickr.com'}])
+
+    Photographer.create([{name: 'Fernando Borges', display_name: "Alvez Pessanha", phone: '22 2888-1231', birthdate: '30-03-1990', city: 'Campos', country: 'Brasil',
+      state: 'RJ', email: 'fernando@mail.com', password: 'fernando', avatar: 'http://localhost:3000/uploads/perfil.jpg',
+      bio: 'Casamentos, bodas e batizado', portfolio_link: 'www.flickr.com'}])
+
+      Photographer.create([{name: 'Fernando Borges', display_name: "Jose Padilha", phone: '22 2888-1231', birthdate: '30-03-1990', city: 'Campos', country: 'Brasil',
+        state: 'RJ', email: 'fernando@mail.com', password: 'fernando', avatar: 'http://localhost:3000/uploads/perfil.jpg',
+        bio: 'Casamentos, bodas e batizado', portfolio_link: 'www.flickr.com'}])
+
+        Photographer.create([{name: 'Fernando Borges', display_name: "Lucas Alves", phone: '22 2888-1231', birthdate: '30-03-1990', city: 'Campos', country: 'Brasil',
+          state: 'RJ', email: 'fernando@mail.com', password: 'fernando', avatar: 'http://localhost:3000/uploads/perfil.jpg',
+          bio: 'Casamentos, bodas e batizado', portfolio_link: 'www.flickr.com'}])
+
+          Photographer.create([{name: 'Fernando Borges', display_name: "Leandro Vicente", phone: '22 2888-1231', birthdate: '30-03-1990', city: 'Campos', country: 'Brasil',
+            state: 'RJ', email: 'fernando@mail.com', password: 'fernando', avatar: 'http://localhost:3000/uploads/perfil.jpg',
+            bio: 'Casamentos, bodas e batizado', portfolio_link: 'www.flickr.com'}])
