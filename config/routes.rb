a6b433aea2e523ffb42d1dc4b012c3a047bd8acd
Leandro <lleandrovicente@gmail.com>
2016-10-06 12:52:18 -0300

    Refatoramento

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post '/photographers/new_quote', to: 'photographers#new_quote'
   post '/photographers/review_quotes', to: 'photographers#review_quotes'
   post '/clients/review', to: 'clients#review'
+  post '/clients/my_reviews'
   get '/price_quotes/:photographer_id/new', to: 'price_quotes#new'
 
   resources :photographers
@@ -10,7 +11,7 @@ Rails.application.routes.draw do
   resources :photographer_reviews
   resources :client_reviews
   resources :price_quotes
-  root 'home#index'
-  get '/home', to: 'home#index'
+  root 'profile#index'
+  get '/home', to: 'profile#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
