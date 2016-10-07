e2e537924576bd2a7e65ddab5b2a6fbd1a290cec
Nirvana16 <zedanilo161@gmail.com>
2016-10-07 09:20:57 -0300

    alterador redirec_to do fotografo review

--- app/controllers/photographer_reviews_controller.rb
+++ app/controllers/photographer_reviews_controller.rb
@@ -3,7 +3,8 @@ class PhotographerReviewsController < ApplicationController
  def destroy
  	@review = PhotographerReview.find(params[:id])
     @review.destroy
-    redirect_to {:back, notice: 'Review apagado' }
+    # redirect_to {:back, notice: 'Review apagado' }
+    redirect_to :back
   end
 
 end
